9e7dea6ded1712c2d17fbc01b02f040be52eb5a2
Jose Acosta <jdap.dmc@gmail.com>
2022-01-31 21:49:25 -0400

    feat: Banner

--- src/axios/request.js
+++ src/axios/request.js
@@ -1,13 +1,13 @@
 import API_KEY from './config';
 
 const requests = {
+	'Netflix Originals': `/discover/tv?api_key=${API_KEY}&with_networks=213`,
 	Trending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-	NetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
 	TopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
-	ActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-	ComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-	HorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-	RomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+	'Action Movies': `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+	'Comedy Movies': `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+	'Horror Movies': `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+	'Romance Movies': `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
 	Documentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
 };
 
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import axios from '../../axios';
+
+import './Banner.css';
+
+const Banner = ({ endParam }) => {
+	const [movie, setMovie] = useState([]);
+	const backgroundImg = {
+		backgroundImage: `
+        url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})
+        `
+	};
+
+	useEffect(() => {
+		async function getMovie() {
+			const response = await axios.get(endParam);
+			setMovie(
+				response.data.results[
+					Math.floor(
+						Math.random() * response.data.results.length -
+							1
+					)
+				]
+			);
+		}
+
+		getMovie();
+	}, [endParam]);
+
+	return (
+		<section className="banner" style={backgroundImg}>
+			<section className="banner-info">
+				<h1>
+					{movie
+						? movie.title ||
+						  movie.name ||
+						  movie.original_name
+						: null}
+				</h1>
+				<div className="banner-buttons">
+					<button>Play</button>
+					<button>My List</button>
+				</div>
+				<div className="banner-desc">
+					<p>
+						{movie && movie.overview
+							? movie.overview.length > 200
+								? movie.overview.substring(0, 150) +
+								  '...'
+								: movie.overview
+							: null}
+					</p>
+				</div>
+			</section>
+			<div className="fade" />
+		</section>
+	);
+};
+
+export default Banner;
