6c899b45901202c92b24b485bcf1b01527d63a24
Jose Acosta <jdap.dmc@gmail.com>
2022-01-31 22:51:47 -0400

    fix: class header dark"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
 	}, []);
 
 	return (
-		<header className={`header ${darkbar && 'dark'}`}>
+		<header className={`header ${darkbar ? 'dark' : ''}`}>
 			<div className="logo">
 				<img src={Image} alt="Netflix logo" />
 			</div>
